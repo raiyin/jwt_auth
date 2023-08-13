b8c53b6d6ccf5df5da12934f8bd57231f6856445
Maxim P <raiyin@ya.ru>
2023-08-13 14:41:36 +0300

    Controller created

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,53 @@
+class UserController {
+
+    async registration(req, res, next) {
+        try {
+
+        } catch (e) {
+
+        }
+    }
+
+    async login(req, res, next) {
+        try {
+
+        } catch (e) {
+
+        }
+    }
+
+    async lougout(req, res, next) {
+        try {
+
+        } catch (e) {
+
+        }
+    }
+
+    async activate(req, res, next) {
+        try {
+
+        } catch (e) {
+
+        }
+    }
+
+    async refresh(req, res, next) {
+        try {
+
+        } catch (e) {
+
+        }
+    }
+
+    async getusers(req, res, next) {
+        try {
+            res.json(['123', '345']);
+        } catch (e) {
+
+        }
+    }
+}
+
+
+module.exports = new UserController();
