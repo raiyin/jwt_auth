86ca169873e5a02348f283fc7caa545bb3a87626
Maxim P <raiyin@ya.ru>
2023-08-14 23:32:13 +0300

    Refresh token is done

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -12,6 +12,24 @@ class TokenService {
         };
     }
 
+    validateAccessToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+            return userData;
+        } catch (error) {
+            return null;
+        }
+    }
+
+    validateRefreshToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+            return userData;
+        } catch (error) {
+            return null;
+        }
+    }
+
     async saveToken(userId, refreshToken) {
 
         // Здесь немного хромает логика. При попытке залогиниться
@@ -28,6 +46,11 @@ class TokenService {
 
     async removeToken(refreshToken) {
         const tokenData = await tokenModel.deleteOne({ refreshToken });
+        return tokenData;
+    }
+
+    async findToken(refreshToken) {
+        const tokenData = await tokenModel.findOne({ refreshToken });
         return tokenData;
     }
 
