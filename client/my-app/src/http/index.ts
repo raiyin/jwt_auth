79d91149705b57c7163e716bb7276e315b6b75ea
Maxim P <raiyin@ya.ru>
2023-08-16 23:01:33 +0300

    Intercepting request

--- client/my-app/src/http/index.ts
+++ client/my-app/src/http/index.ts
@@ -6,3 +6,9 @@ const $api = axios.create({
     withCredentials: true,
     baseURL: API_URL
 });
+
+
+$api.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+    return config;
+});
