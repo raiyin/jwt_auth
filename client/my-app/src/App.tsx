d3539601d0c84a66936a2970ed5070c30be8dc3c
Maxim P <raiyin@ya.ru>
2023-08-17 17:41:28 +0300

    Mobx and global state

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div>
-      123
+      <LoginForm />
     </div>
   );
 }
--- client/my-app/src/components/LoginForm.tsx
+++ client/my-app/src/components/LoginForm.tsx
@@ -1,9 +1,11 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useContext, useState } from 'react';
+import { Context } from '..';
 
 const LoginForm: FC = () => {
 
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const { store } = useContext(Context);
 
     return (
         <div>
@@ -19,8 +21,12 @@ const LoginForm: FC = () => {
                 value={password}
                 placeholder='Пароль'
             />
-            <button>Login</button>
-            <button>Registration</button>
+            <button onClick={() => store.login(email, password)}            >
+                Login
+            </button>
+            <button onClick={() => store.registration(email, password)}            >
+                Registration
+            </button>
         </div>
     );
 };
